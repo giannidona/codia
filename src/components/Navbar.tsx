2e96196f691b4f082f1151bd6874336343127407
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-08-28 23:29:49 -0300

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,68 @@
-import { AlignJustify } from "lucide-react";
+"use client";
+
+import { useState } from "react";
+import { AlignJustify, X } from "lucide-react";
 import Image from "next/image";
 
-const links = [{ link: "" }, { link: "" }, { link: "" }, { link: "" }];
+const links = [
+  { slug: "/awd", text: "Inicio" },
+  { slug: "/aa", text: "Servicios" },
+  { slug: "/ww", text: "Proyectos" },
+  { slug: "/dd", text: "Contacto" },
+];
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="absolute flex h-[75px] w-full bg-black/90 backdrop-blur-sm">
       <div className="mx-auto flex w-[90%] items-center justify-between">
-        <div className="">
-          <Image src={"/logo.svg"} alt="codia logo" width={100} height={200} />
-        </div>
         <div>
-          <AlignJustify className="text-white" size={30} />
+          <Image src="/logo.svg" alt="codia logo" width={100} height={50} />
         </div>
-        <div className="hidden">
-          <ul className="flex">
-            <li>
-              <a href="/">Inicio</a>
-            </li>
-            <li>
-              <a href="/">Servicios</a>
-            </li>
-            <li>
-              <a href="/">Proyectos</a>
-            </li>
-            <li>
-              <a href="/">Contacto</a>
-            </li>
+        <div className="flex md:hidden">
+          <button onClick={toggleMenu} aria-label="Toggle menu">
+            {isOpen ? (
+              <X className="text-white" size={30} />
+            ) : (
+              <AlignJustify className="text-white" size={30} />
+            )}
+          </button>
+        </div>
+        <div className="hidden md:flex">
+          <ul className="flex space-x-4">
+            {links.map((link) => (
+              <li key={link.slug}>
+                <a href={link.slug} className="text-white hover:text-gray-400">
+                  {link.text}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
+      </div>
+      <div
+        className={`fixed inset-0 top-[75px] bg-black/90 p-4 text-right ${
+          isOpen ? "flex" : "hidden"
+        }`}
+      >
+        <ul className="flex w-[97%] flex-col space-y-4">
+          {links.map((link) => (
+            <li key={link.slug}>
+              <a
+                href={link.slug}
+                className="text-white hover:text-gray-400"
+                onClick={() => setIsOpen(false)}
+              >
+                {link.text}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
