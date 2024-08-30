f325ccee06f8cb797b1b3e3bee82c2d6890929b8
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-08-30 00:02:37 -0300

    components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Hero } from "@/components";
-import { Services } from "@/components";
+import { Contact, Hero, Services } from "@/components";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Services />
+      <Contact />
     </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Navbar } from "./Navbar";
 export { Hero } from "./Hero";
 export { Services } from "./Services";
 export { CardProyect } from "./CardProyect";
+export { Contact } from "./Contact";
